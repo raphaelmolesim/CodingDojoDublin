958b8ed75e0701ed62805e050b3d5f06fce0d412
Raphael Molesim <raphael.sm86@gmail.com>
2010-10-27 22:20:16 +0100

    We have the magic hypothenuse working! Raphael and Gavin

--- sunny_montains/sunny_montain.rb
+++ sunny_montains/sunny_montain.rb
@@ -12,20 +12,32 @@ class SunnyMontain
 		Math.sqrt(square1 + square2)	
 	end
 	
-	def length_of_shadow
-		i=0
+	def length_of_sunshine
+		i = 0
 		total_len = 0.0
+		max_y = -1
 		
 		(points.size-1).times do
-		  total_len += hyp(points[i], points[i+1])
-		  i=i+1
+			puts(total_len)
+			if (max_y < points[i+1].y)
+				
+				max_y = points[i+1].y
+		  	total_len += hyp(points[i], points[i+1]) 
+		  end
+		  i = i + 1
 		end
 		
-		total_len
-		
+		total_len		
 	end
 
 	def initialize()
 		@points = []
+	end
+	
+	def magic_hypothenuse(p1, p2, y)
+		hyp1 = hyp(p1, p2)
+		height1 = p1.y.to_f - p2.y.to_f
+		height2 = p1.y.to_f - y
+		(hyp1 * height2)/height1
 	end
 end
--- sunny_montains/sunny_montain_spec.rb
+++ sunny_montains/sunny_montain_spec.rb
@@ -30,14 +30,40 @@ describe SunnyMontain do
 		@sunny.hyp(point1, point2).should be == Math.sqrt(5)
 	end
 	
-	it "should calculating length of shadow" do
+	it "should calculating length of sunshine" do
 		@sunny.points << Point.new(10, 0)
 		@sunny.points << Point.new(8, 1)
 		@sunny.points << Point.new(6, 2)
 		
-		@sunny.length_of_shadow.should be == Math.sqrt(5) + Math.sqrt(5)
+		@sunny.length_of_sunshine.should be == Math.sqrt(5) + Math.sqrt(5)
 	end
 	
+	it "should calculating length of sunshine with a point cover by the shadow " do
+		@sunny.points << Point.new(10, 0)
+		@sunny.points << Point.new(8, 1)
+		@sunny.points << Point.new(6, 0)
+		
+		@sunny.length_of_sunshine.should be == Math.sqrt(5)
+	end
 	
+	it "should calculate a complex path with both hidden and visible sides" do
+		pending
+		@sunny.points << Point.new(10, 0)
+		@sunny.points << Point.new(8, 1)
+		@sunny.points << Point.new(6, 0)
+		@sunny.points << Point.new(4, 2)
+		
+		@sunny.length_of_sunshine.should be == Math.sqrt(5) + Math.sqrt(5)
+	end
+	
+	it "should deliver magic power" do
+    y = 1
+		p1 = Point.new(6, 0)
+		p2 = Point.new(4, 2)
+    
+    hyp = @sunny.magic_hypothenuse(p1, p2, y)
+
+    hyp.should be == Math.sqrt(2)
+	end
 
 end
