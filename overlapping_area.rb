2c15b8d433e494ad82fce584d7e3634c55cdebde
Raphael Molesim <raphael.sm86@gmail.com>
2011-02-16 20:37:41 +0000

    test for overlapping area of 2

--- overlapping_area.rb
+++ overlapping_area.rb
@@ -4,7 +4,8 @@ class OverlappingArea
   
 end
 
-def overlapping_area (rectangle1=nil, rectangle2=nil)
-  return 1 if rectangle1
-  0
+def overlapping_area (rectangle1=[], rectangle2=[])
+  return 0 if rectangle1 and rectangle2
+  return 2 if rectangle1[2] <= rectangle2[2] or rectangle1[3] <= rectangle2[3]
+  1
 end
--- spec/overlapping_area_spec.rb
+++ spec/overlapping_area_spec.rb
@@ -14,4 +14,8 @@ describe OverlappingArea do
     overlapping_area([0,0,2,2], [-1,-1,1,1]).should == 1
   end
   
+  it "should return 2 if I have an overlapping area of 2" do
+    overlapping_area([0,0,2,2], [0,0,2,1]).should == 2
+  end
+  
 end
