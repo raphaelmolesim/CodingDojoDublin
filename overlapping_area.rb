59ee8ce0db4b37b0edd09d0fec43196233afc4ab
Raphael Molesim <raphael.sm86@gmail.com>
2011-02-16 20:26:43 +0000

    Test passing with 2 rectangle overlapping

--- overlapping_area.rb
+++ overlapping_area.rb
@@ -3,3 +3,8 @@ class OverlappingArea
   
   
 end
+
+def overlapping_area (rectangle1=nil, rectangle2=nil)
+  return 1 if rectangle1
+  0
+end
--- spec/overlapping_area_spec.rb
+++ spec/overlapping_area_spec.rb
@@ -3,7 +3,15 @@ require "overlapping_area"
 describe OverlappingArea do
   
   it "should be to able to createa instance" do
-    OverlappingArea.class.should be == OverlappingArea
+    OverlappingArea.new.should be_an OverlappingArea
+  end
+  
+  it "should return zero if I don't have retangles" do
+    overlapping_area.should == 0
+  end
+  
+  it "should return 1 if I have an overlapping area of 1" do
+    overlapping_area([0,0,2,2], [-1,-1,1,1]).should == 1
   end
   
 end
