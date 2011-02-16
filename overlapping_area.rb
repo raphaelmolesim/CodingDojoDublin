1142d8d37c33c13caa0154b0512b467edee79318
Raphael Molesim <raphael.sm86@gmail.com>
2011-02-16 20:15:42 +0000

    Set environment

---
+++ overlapping_area.rb
@@ -0,0 +1,5 @@
+class OverlappingArea
+  
+  
+  
+end
---
+++ spec/overlapping_area_spec.rb
@@ -0,0 +1,9 @@
+require "overlapping_area"
+
+describe OverlappingArea do
+  
+  it "should be to able to createa instance" do
+    OverlappingArea.class.should be == OverlappingArea
+  end
+  
+end
